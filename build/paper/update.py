1c1240851136ebdfa6886e9bf4fccb3830629efa
Simon Zapf <s.zapf@onlineprinters.com>
2022-10-18 15:22:46 +0200

    persist current version in update.ini

--- build/paper/update.py
+++ build/paper/update.py
@@ -2,11 +2,13 @@
 
 import requests
 import os
+import configparser
 from argparse import ArgumentParser
 from urllib import request
 from time import sleep, time
 from tqdm import tqdm
 from yaspin import yaspin
+from pathlib import Path
 
 RESTART_TIME = 10
 BASE_URL = 'https://api.papermc.io/v2'
@@ -15,6 +17,9 @@ SERVER_PATH = '../../server'
 BACKUP_PATH = '../../backup'
 START_SERVER_CMD = 'systemctl start minecraft'
 STOP_SERVER_CMD = 'systemctl stop minecraft'
+CONFIG_FILE = 'update.ini'
+USER = 'minecraft'
+GROUP = 'minecraft'
 
 
 class DownloadProgressBar(tqdm):
@@ -24,12 +29,19 @@ class DownloadProgressBar(tqdm):
         self.update(b * bsize - self.n)
 
 
+def create_config():
+    filename = Path(CONFIG_FILE)
+    filename.touch(exist_ok=True)
+
+
+@yaspin(text='Fetching latest version info')
 def get_latest_version():
     versions_url = f'{BASE_URL}/projects/paper'
     versions = requests.get(versions_url).json()['versions']
     return versions.pop()
 
 
+@yaspin(text='Fetching latest build info')
 def get_latest_build(v):
     builds_url = f'{BASE_URL}/projects/paper/versions/{v}/builds'
     builds = requests.get(builds_url).json()['builds']
@@ -77,6 +89,24 @@ if __name__ == '__main__':
         if download_file is None:
             download_file = get_latest_build(version)['downloads']['application']['name']
 
+        create_config()
+
+        config = configparser.ConfigParser()
+        config.read(CONFIG_FILE)
+
+        try:
+            current_version = config['Settings']['current_version']
+            current_build = config['Settings']['current_build']
+        except KeyError as e:
+            current_version = ''
+            current_build = ''
+
+        if str(current_build) == str(build) and str(current_version) == str(version):
+            print('There is no update available.')
+            print(f'Current version {current_version}')
+            print(f'Current build: {current_build}')
+            exit()
+
         download_url = f'{BASE_URL}/projects/paper/versions/{version}/builds/{build}/downloads/{download_file}'
         download(download_url)
 
@@ -90,11 +120,21 @@ if __name__ == '__main__':
         os.system(f'mv {FILENAME} {SERVER_PATH}/{FILENAME}')
         print('Installed new server.jar version')
 
-        os.system(f'chown minecraft:minecraft {SERVER_PATH}/{FILENAME}')
+        os.system(f'chown {USER}:{GROUP} {SERVER_PATH}/{FILENAME}')
 
         start_server()
 
         print('\nSuccessfully installed latest paper server')
+
+        config['Settings'] = {
+            'current_version': version,
+            'current_build': build
+        }
+
+        with open(CONFIG_FILE, 'w') as configfile:
+            config.write(configfile)
+
+        print('Updated update.ini')
 
     except Exception as e:
         print(f'An error occurred while fetching {e}\n')
