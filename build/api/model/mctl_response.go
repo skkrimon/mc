fdd9247724e94a596a04f42904ddbdf80eac9580
Simon Zapf <simon-zapf@hotmail.de>
2022-11-02 13:59:30 +0100

    move api to seperate project

--- build/api/model/mctl_response.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type MctlResponse struct {
-	Success bool   `json:"success"`
-	Message string `json:"message"`
-}
--- build/api/routes/index.go
+++
@@ -1,8 +0,0 @@
-package routes
-
-import "github.com/gin-gonic/gin"
-
-func AddRoutes(superRoute *gin.RouterGroup) {
-	PingRoutes(superRoute)
-	MctlRoutes(superRoute)
-}
